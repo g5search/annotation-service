c54ba21e6e70dde234e0bf0ccdbaa04fd6d4bf69
David Miller <david.miller@getg5.com>
2020-06-05 11:08:23 -0700

    staging

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 module.exports = {
   mode: 'spa',
   server: {
@@ -10,7 +12,11 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      {
+        hid: 'description',
+        name: 'description',
+        content: process.env.npm_package_description || ''
+      }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
@@ -48,6 +54,8 @@ module.exports = {
     bootstrapVueCSS: false
   },
   axios: {
+    debug: true,
+    https: true,
     browserBaseURL: `//${process.env.BROWSER_URL}`
   },
   googleAnalytics: {
--- pages/index.vue
+++ pages/index.vue
@@ -2,17 +2,14 @@
   <b-container fluid class="px-5">
     <div class="ceph-container">
       <octopus
-        :size="`8.5em`"
+        :size="`7.25em`"
         :color="`#334159`"
-        class="ceph-container__svg shadowed"
+        class="ceph-container__svg"
       />
       <div class="ceph-container__title text-primary">
         <b-img-lazy
           src="/g5-logo.png"
         />
-        <span class="text-white">
-          Notes
-        </span>
       </div>
     </div>
     <b-row no-gutters>
@@ -55,12 +52,6 @@ export default {
   components: {
     Octopus
   },
-  data() {
-    return {
-      strategist: null,
-      strategists: []
-    }
-  },
   async asyncData({ $axios }) {
     const reject = [
       'annotation',
@@ -68,13 +59,20 @@ export default {
       'annotationTypeId',
       'external_id'
     ]
-    const notes = await $axios.$get('https://notes.g5marketingcloud.com/api/v1/notes')
+    // const notes = await $axios.$get('https://notes.g5marketingcloud.com/api/v1/notes')
+    const notes = await $axios.$get('api/v1/notes')
     return {
       notes,
       fields: Object.keys(notes[0]).map(key => ({
         key,
         sortable: true
       })).filter(field => !reject.includes(field.key))
+    }
+  },
+  data() {
+    return {
+      strategist: null,
+      strategists: []
     }
   }
 }
@@ -85,8 +83,6 @@ export default {
   position: fixed;
   top: 0%;
   left: 0%;
-  // width: 100%;
-  // transform: translate(0%, 0%);
   z-index: 5;
   &__svg {
     position: absolute;
@@ -95,27 +91,14 @@ export default {
     transform: rotate(-45deg) translateX(-5%);
   }
   &__title {
-    // position: absolute;
     top: 50%;
     left: 50%;
-    // width: 5rem;
     transform: translate(25%, 50%);
     font-size: 1.5rem;
     padding: 0.05em 0.35em;
-    border: 2.5px solid white;
-    background-color: rgba(47, 56, 176, 1);
-    border-radius: 20px;
-    box-shadow: 0px 5px 20px rgba(0, 0, 0, 0.5);
-    // background-color: white;
     display: flex;
     align-items: center;
     justify-content: flex-start;
   }
-}
-.shadowed {
-  -webkit-filter: drop-shadow(0px 0px 20px rgba(0,0,0,1));
-  filter: url('/drop-shadow.svg#drop-shadow');
-  -ms-filter: "progid:DXImageTransform.Microsoft.Dropshadow(OffX=12, OffY=12, Color='#444')";
-  filter: "progid:DXImageTransform.Microsoft.Dropshadow(OffX=12, OffY=12, Color='#444')";
 }
 </style>
