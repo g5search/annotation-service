dc0039fa8c12e544c0fb19057ef5f64a1e682c01
David Miller <david.miller@getg5.com>
2020-06-19 09:06:30 -0700

    ready enough

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@
       <octopus
         :size="`6em`"
         :color="`#334159`"
-        :class="[{ 'is-open': isOpen }, 'ceph-container__svg']"
+        :class="[{ 'is-open': isOpen }, 'ceph-container__svg', 'shadowed']"
       />
       <div class="ceph-container__title text-primary">
         <b-img-lazy
@@ -82,7 +82,8 @@
             />
           </b-form-group>
           <b-btn-group class="w-100">
-            <b-btn @click="onSubmit">
+            <b-btn @click="onUpdate">
+              <b-spinner v-if="isBusy" small />
               Update Table
             </b-btn>
             <b-btn
@@ -95,7 +96,7 @@
         </b-collapse>
       </b-card>
       <b-card
-        header-class="d-flex m-0 p-0 justify-content-between align-items-center"
+        header-class="d-flex m-0 p-0 justify-content-start align-items-end border-0"
         body-class="p-1"
       >
         <template v-slot:header>
@@ -122,7 +123,8 @@
         <b-card
           bg-variant="white"
           body-class="p-3"
-          header-class="d-flex"
+          header-class="d-flex primary-header"
+          header-bg-variant="neutral"
         >
           <template v-slot:header>
             <b-input-group>
@@ -146,7 +148,7 @@
                 </b-btn>
               </template>
             </b-input-group>
-            <b-input-group class="mx-2">
+            <b-input-group class="ml-2">
               <template v-slot:prepend>
                 <b-input-group-text>
                   Per Page
@@ -161,18 +163,29 @@
               v-model="currentPage"
               :total-rows="totalRows"
               :per-page="perPage"
-              class="my-0 mr-2"
+              class="my-0 mx-2"
             />
             <b-btn
+              id="download-csv-btn"
+              :href="downloadCsv"
+              download="notes.csv"
               variant="primary"
+              class="d-flex align-items-center mr-2"
             >
               <b-icon-download />
+              <span class="ml-2">
+                CSV
+              </span>
             </b-btn>
             <b-btn
-              variant="primary"
               @click="isOpen = !isOpen"
+              variant="primary"
+              class="d-flex align-items-center"
             >
-              <b-icon-caret-right />
+              <b-icon-chevron-double-right />
+              <span class="ml-2">
+                More
+              </span>
             </b-btn>
           </template>
           <b-table
@@ -185,11 +198,10 @@
             small
             striped
             sticky-header
-            bordered
           >
             <template v-slot:cell(internal)="row">
               <div class="hover-anchor">
-                <b-icon-emoji-neutral font-scale="2" v-if="row.item.internal" />
+                <b-icon-emoji-neutral v-if="row.item.internal" font-scale="2" />
                 <b-icon-emoji-sunglasses v-else font-scale="2" />
                 <div class="hovered-icon">
                   {{ row.item.internal ? 'Internal Only' : 'Customer-Facing' }}
@@ -201,18 +213,67 @@
               <b-form-text
                 v-if="row.item.startDate"
               >
-                {{ row.item.startDate }} - {{ row.item.endDate }}
+                <b-row no-gutters>
+                  <b-col cols="6">
+                    Start
+                  </b-col>
+                  <b-col cols="6">
+                    End
+                  </b-col>
+                </b-row>
+                <b-row no-gutters>
+                  <b-col cols="6">
+                    {{ row.item.startDate }}
+                  </b-col>
+                  <b-col cols="6">
+                    {{ row.item.endDate }}
+                  </b-col>
+                </b-row>
               </b-form-text>
             </template>
-            <template v-slot:cell(html)="row">
-              <read-only :content="row.item.html" />
+            <template v-slot:cell(createdAt)="row">
+              {{ new Date(row.item.createdAt).toLocaleDateString() }}
             </template>
-            <template v-slot:cell(edit)>
+            <template v-slot:cell(updatedAt)="row">
+              {{ new Date(row.item.updatedAt).toLocaleDateString() }}
+            </template>
+            <template v-slot:cell(locations)="row">
+              <div v-if="row.item.locations.length >= 10">
+                {{ row.item.locations.length }} Locations
+              </div>
+              <b-badge
+                v-else
+                v-for="(loc, i) in row.item.locations"
+                :key="loc"
+                :variant="`primary-${i}`"
+                class="mr-1"
+              >
+                {{ loc }}
+              </b-badge>
+            </template>
+            <template v-slot:cell(salesforceSync)="row">
+              <b-icon-check-circle-fill v-if="row.item.salesforceSync" scale="1.5" />
+              <b-icon-x-circle-fill v-else scale="1.5" />
+            </template>
+            <template v-slot:cell(note)="row">
+              <read-only :content="row.item.note" />
+            </template>
+            <template v-slot:cell(edit)="row">
               <b-btn
+                @click="onToggle(row)"
                 variant="outline-primary"
+                style="vertical-align: text-top;"
+                class="d-flex"
               >
                 <b-icon-pencil-square />
+                <span class="ml-2">
+                  Edit
+                </span>
               </b-btn>
+            </template>
+            <template v-slot:row-details="row">
+              {{ row }}
+              <!-- <inline-editor :row="row" @toggle-details="onToggle" /> -->
             </template>
           </b-table>
         </b-card>
@@ -225,17 +286,17 @@
 import Octopus from '~/components/icons/octopus'
 import ReadOnly from '~/components/read-only'
 import NoteEditor from '~/components/note-editor'
+import PapaMixin from '~/mixins/papaparse'
 export default {
   components: {
     Octopus,
     NoteEditor,
     ReadOnly
   },
+  mixins: [PapaMixin],
   async asyncData({ $axios }) {
     const reject = [
       'annotation',
-      'annotationCategoryId',
-      'annotationTypeId',
       'external_id',
       'startDate',
       'endDate'
@@ -244,12 +305,6 @@ export default {
     const strategists = await $axios.$get('api/v1/strategists')
     // const clients = await $$axios.$get('api/v1/clients')
     return {
-      notes,
-      totalRows: notes.length,
-      strategists: strategists.map(s => ({
-        text: `${s.first_name} ${s.last_name}`,
-        value: s.email
-      })),
       fields: [
         ...Object.keys(notes[0])
           .map(key => ({
@@ -258,14 +313,21 @@ export default {
             class: 'text-center align-middle'
           })).filter(field => !reject.includes(field.key)),
         { key: 'Edit', class: 'text-center align-middle' }
-      ]
+      ],
+      notes,
+      totalRows: notes.length,
+      strategists: strategists.map(s => ({
+        text: `${s.first_name} ${s.last_name}`,
+        value: s.email
+      }))
     }
   },
   data() {
     return {
       strategist: null,
-      isOpen: true,
+      isOpen: false,
       isBusy: false,
+      isError: false,
       currentPage: 1,
       perPage: 10,
       pageOptions: [10, 20, 50, 100],
@@ -293,9 +355,20 @@ export default {
     onClear() {
       this.search = ''
     },
-    onSubmit(evt) {
-      // const endpoint = `api/v1/notes?userEmail=${this.strategist.value}`
+    onDrop(row) {},
+    onToggle(row) {
+      row.toggleDetails()
+      // this.refetch()
+    },
+    onUpdate(evt) {
+      const endpoint = `api/v1/notes?userEmail=${this.strategist}`
       this.isBusy = true
+      this.$axios
+        .$get(endpoint)
+        .then((res) => {})
+        .catch(() => {
+          this.isError = true
+        })
     }
   }
 }
@@ -334,10 +407,28 @@ export default {
     }
   }
 }
+.primary-header {
+  box-shadow: 0 5px 25px rgba(25, 53, 106, 0.5);
+}
+.align-middle {
+  vertical-align: middle;
+}
+.slide-fade-enter-active,
+.slide-fade-leave-active {
+  transition: all 400ms cubic-bezier(0.2, 0.4, 0.8, 1.0);
+}
+.slide-fade-enter {
+  transform: translateY(-20px);
+  opacity: 0;
+}
+.slide-fade-leave-to {
+  transform: translateY(20px);
+  opacity: 0;
+}
 .hover-anchor {
   position: relative;
   background-color: inherit;
-  transition: opacity 200ms ease-in-out;
+  transition: 200ms ease-in-out;
   border-radius: 20px / 50% 0 0 50%;
   height: calc(100% + 10px);
   padding: 5px 0;
@@ -355,7 +446,8 @@ export default {
     border-radius: 20px / 0 50% 50% 0;
   }
   &:hover {
-    background-color: #f1eaea;
+    background-color: #19356a;
+    color: white;
     & .hovered-icon {
       opacity: 1;
     }
