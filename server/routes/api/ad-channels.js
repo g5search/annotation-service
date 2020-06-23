1b22587a55be85cb1cfa6ba8d676b9539216e31a
David Miller <david.miller@getg5.com>
2020-06-23 12:01:42 -0700

    remove unused

--- server/routes/api/ad-channels.js
+++ server/routes/api/ad-channels.js
@@ -1,7 +1,6 @@
 const cors = require('cors')
 const axios = require('axios')
 const { CRS_URL: crsUrl } = process.env
-// const models = require('../../models')
 const whitelist = [/chrome-extension:\/\/[a-z]*$/]
 const corsOpts = {
   origin: (origin, callback) => {
@@ -27,12 +26,14 @@ module.exports = (app) => {
     const { data } = await axios.get(`${crsUrl}/api/dam/code_account/${accountId}`)
     res.json(data)
   })
+
   app.options('/api/v1/google-ads/:accountId', cors(corsOpts))
   app.get('/api/v1/google-ads/:accountId', cors(corsOpts), async (req, res) => {
     const { accountId } = req.params
     const { data } = await axios.get(`${crsUrl}/api/dam/code_account/${accountId}`)
     res.json(data)
   })
+
   app.options('/api/v1/microsoft/account/:accountId')
   app.get('/api/v1/microsoft/:accountId', cors(corsOpts), async (req, res) => {
     const { accountId } = req.params
