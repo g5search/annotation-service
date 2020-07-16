2d00c89b6297b20b2ea7daa3aff1354cfb1b95cd
Tyler Hasenoehrl <tylrhas@gmail.com>
2020-07-16 15:59:32 -0700

    change queue ui

--- server/controllers/queue/index.js
+++ server/controllers/queue/index.js
@@ -1,23 +1,26 @@
 const path = require('path')
 const fs = require('fs')
 const Bull = require('bull')
-const { setQueues, UI } = require('bull-board')
-
-function init(app) {
+const Arena = require('bull-arena')
+function init (app) {
   const queues = []
   fs.readdirSync(__dirname)
-    .filter(file => file.indexOf('.') !== 0 && file !== 'index.js' && file !== 'README.md')
+    .filter(file => file.indexOf('.') !== 0 && file !== 'index.js' && file !== 'README.md') // get all the model files
     .forEach((file) => {
       const queue = require(path.join(__dirname, file))
       const fileName = file.replace('.js', '')
-      const queueFile = queue(Bull)
-      console.log({ fileName })
-      module.exports[fileName] = queueFile
-      queues.push(queueFile)
+      module.exports[fileName] = queue.bullConfig(Bull)
+      queues.push(queue.arenaConfig())
     })
 
-  setQueues(queues)
-  app.use('/admin/queues', UI)
+  const arenaConfigs = Arena({
+    queues
+  },
+  {
+    basePath: '/admin/queues',
+    disableListen: true
+  })
+  app.use('/', arenaConfigs)
 }
 
 module.exports = {
--- server/controllers/queue/salesforce.js
+++ server/controllers/queue/salesforce.js
@@ -4,10 +4,23 @@ const sfRemoveNote = path.resolve('./server/controllers/jobs/removeNote')
 const sfUpdateNote = path.resolve('./server/controllers/jobs/updateNote')
 const { REDIS_URL } = process.env
 
-module.exports = (Bull) => {
+module.exports = {
+  bullConfig,
+  arenaConfig
+}
+
+function bullConfig (Bull) {
   const salesforce = new Bull('salesforce', REDIS_URL)
   salesforce.process('sync', 1, sfCreateNote)
   salesforce.process('remove', 1, sfRemoveNote)
   salesforce.process('update', 1, sfUpdateNote)
   return salesforce
+}
+
+function arenaConfig () {
+  return {
+    name: 'salesforce',
+    hostId: 'notes',
+    url: REDIS_URL
+  }
 }
