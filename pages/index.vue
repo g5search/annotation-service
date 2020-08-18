4aef3e05c95e3e9a88bd8c9d200cf66b3e1fda64
David Miller <david.miller@getg5.com>
2020-08-18 14:12:24 -0700

    include id in export

--- pages/index.vue
+++ pages/index.vue
@@ -517,7 +517,6 @@ export default {
       let month = '' + (d.getMonth() + 1)
       let day = '' + d.getDate()
       const year = d.getFullYear()
-
       if (month.length < 2) {
         month = '0' + month
       }
@@ -555,7 +554,7 @@ export default {
     },
     updateCsv() {
       const columns = [
-        'id',
+        // 'id',
         'internal',
         'category',
         'annotationType',
