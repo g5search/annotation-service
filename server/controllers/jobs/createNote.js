d05ad25a92b55985e562d6a74568ee916d5b6325
Tyler Hasenoehrl <tylrhas@gmail.com>
2020-10-05 15:37:33 -0700

    add sync for added loc

--- server/controllers/jobs/createNote.js
+++ server/controllers/jobs/createNote.js
@@ -39,10 +39,12 @@ module.exports = async function (job, sfApi) {
     .replace(/[\r\t\f\v ]{2,}/g, '').trim()
   if (locationUrns.length > 0) {
     for (let i = 0; i < locationUrns.length; i++) {
-      console.log(locationUrns[i])
-      const { Id } = await sfApi.findLocation({ Location_URN__c: locationUrns[i] }, ['Id'])
-      const { id: noteId } = await sfApi.createNote(Id, userId, annotationCategory.dataValues.name, annotationType ? annotationType.dataValues.name : null, internal, text, moment(createdAt).format('YYYY-MM-DD'), 'Completed', annotationCategory.dataValues.name, taskType)
-      await dbAnnotation.g5_updatable_locations[i].annotationLocation.update({ salesforce_id: noteId })
+      if (!dbAnnotation.g5_updatable_locations[i].annotationLocation.salesforce_id) {
+        console.log(locationUrns[i])
+        const { Id } = await sfApi.findLocation({ Location_URN__c: locationUrns[i] }, ['Id'])
+        const { id: noteId } = await sfApi.createNote(Id, userId, annotationCategory.dataValues.name, annotationType ? annotationType.dataValues.name : null, internal, text, moment(createdAt).format('YYYY-MM-DD'), 'Completed', annotationCategory.dataValues.name, taskType)
+        await dbAnnotation.g5_updatable_locations[i].annotationLocation.update({ salesforce_id: noteId })
+      }
     }
   } else {
     const { Id } = await sfApi.findAccount({ Client_URN__c: g5_updatable_client.dataValues.urn }, ['Id'])
--- server/models/primary/hooks/annotation-location.js
+++ server/models/primary/hooks/annotation-location.js
@@ -6,4 +6,11 @@ module.exports = (models) => {
   models.annotationLocation.addHook('afterDestroy', async (instance, options) => {
     await salesforce.add({ type: 'remove', ...instance.dataValues })
   })
+  models.annotationLocation.addHook('afterCreate', async (instance, options) => {
+    // create a job to sync the data to SF
+    const { annotationId } = instance.dataValues
+    const annotation = await models.annotation.findOne({ id: annotationId })
+    await annotation.update({ salesforceSync: false })
+    await salesforce.add({ type: 'sync', id: annotationId })
+  })
 }
